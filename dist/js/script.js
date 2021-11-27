ff65a71d91a34891a364262c4948da88980def36
Oleg <olegjan9@rambler.ru>
2021-11-27 18:04:03 +0300

    27.11.2021

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,18 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => { /* После полной загрузки HTML выполняется JS */
+document.addEventListener("DOMContentLoaded", () => {
+    /* После полной загрузки HTML выполняется JS */
 
-    
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        document.body.classList.add("mobile");
+    } else {
+        // код для обычных устройств
+    }
+
+  
+
+
+
+
+
 });
