c988b3a9eb4a618ee780e7ce3a7035157f30c1cb
Oleg <olegjan9@rambler.ru>
2022-01-09 00:24:18 +0300

    09.01.2022

--- dist/js/script.js
+++ dist/js/script.js
@@ -12,22 +12,37 @@ document.addEventListener("DOMContentLoaded", () => {
     //--------------------Слайдер----------------------//
 
     const expertNextSlider = document.querySelector(".expert__next-wapper img"),
-    expertPreviosSlider = document.querySelector(".expert__previos-wapper img"),
-    expertSliders = document.querySelector(".expert__sub-wapper-sliders"),
-    widthExpertSliders = window.getComputedStyle(expertSliders).width;
-    
+        expertPreviosSlider = document.querySelector(".expert__previos-wapper img"),
+        expertSubWapperSliders = document.querySelector(".expert__sub-wapper-sliders"),
+        expertSliders = document.querySelector(".expert__sliders"),
+        expertSlide = document.querySelectorAll(".expert__slide"),
+        width = window.getComputedStyle(expertSubWapperSliders).width;
+
     let offset = 0;
 
-    console.log(widthExpertSliders);
-    // console.log(+Math.round(widthExpertSliders.replace(/\D/g, "")));
+    console.log((+width.replace(/\D/g, "") * (expertSlide.length - 1)));
 
-
-    // )
-
-
-    expertNextSlider.addEventListener("click", (e)=> {
-        // if(offset = +widthExpertSliders)
+    expertNextSlider.addEventListener("click", () => {
+        if (offset == (+width.replace(/\D/g, "") * (expertSlide.length - 1))) {
+            offset = 0
+        } else {
+            offset += +width.replace(/\D/g, "");
+        }
+        expertSliders.style.transform = `translateX(-${offset}px)`;
     })
+
+    expertPreviosSlider.addEventListener("click", () => {
+        if (offset == 0) {
+            offset = (+width.replace(/\D/g, "") * (expertSlide.length - 1))            
+        } else {
+            offset -= +width.replace(/\D/g, "");
+        }
+        expertSliders.style.transform = `translateX(-${offset}px)`;
+    })
+
+
+
+
 
 
 
