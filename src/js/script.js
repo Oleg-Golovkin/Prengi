08ca00874101af6c7a4ffd169ba27f1063c09c68
Oleg <olegjan9@rambler.ru>
2022-01-25 10:11:38 +0300

    24.01.2022

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
 "use strict";
 
 import {viewDevice} from "./modules/view_device";
-// import {slider} from "./modules/slider";
+import {slider} from "./modules/slider";
 import {simpleAdaptiveSliderMin} from "./modules/simple-adaptive-slider.min";
 
 document.addEventListener("DOMContentLoaded", () => {
     viewDevice();
-    // slider();
+    slider();
     simpleAdaptiveSliderMin();
 
 });
