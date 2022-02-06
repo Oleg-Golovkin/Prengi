8fc1793a550ae19460c0528cb127fca214b07d69
Oleg <olegjan9@rambler.ru>
2022-02-06 18:04:35 +0300

    06.02.2022

---
+++ dist/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import {
+    viewDevice
+} from "./modules/view_device";
+import {
+    slider
+} from "./modules/slider";
+
+document.addEventListener("DOMContentLoaded", () => {
+    viewDevice();
+    slider();
+});
