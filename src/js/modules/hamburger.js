f7802095b5e68f0a85dd4664771c8840e97c31c5
Oleg <olegjan9@rambler.ru>
2022-02-13 15:59:00 +0300

    13/02/2022

--- src/js/modules/hamburger.js
+++ src/js/modules/hamburger.js
@@ -1,25 +1,37 @@
 const hamburgerLine = document.querySelectorAll(".hamburger__line"),
-    hamburger = document.querySelector(".hamburger");
+    hamburger = document.querySelector(".hamburger"),
+    menu = document.querySelector(".menu"),
+    menuBackground = document.querySelector(".menu__background");
 
 document.addEventListener("click", (e) => {
 
     //----------------------------active menu------------------------------------//
     function activeHamburger(selector) {
         if (e.target && e.target.matches(selector)) {
-            console.log('ok');
             hamburgerLine.forEach(item => {
                 item.classList.add("hamburger__line_active");
             });
+            menu.style.left = "0px";
+            menuBackground.style.opacity = "1";
+            menuBackground.style.visibility = "visible";
+
         }
     }
     activeHamburger(".hamburger");
     activeHamburger(".hamburger__line");
 
     // ----------------------------deactivation menu --------------------------//
-    if (e.target.matches(selector)) {
-        hamburgerLine.forEach(item => {
-            item.classList.remove("hamburger__line_active");
-        });
-     
+    function deactivationHamburger(selector) {
+        if (e.target.matches(selector)) {
+            hamburgerLine.forEach(item => {
+                item.classList.remove("hamburger__line_active");
+            });
+            menu.style.left = "-200px";
+            menuBackground.style.opacity = "0";
+            menuBackground.style.visibility = "hidden";
+
+        }
     }
+    deactivationHamburger(".menu__background");
+
 });
